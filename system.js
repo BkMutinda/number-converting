33a1aa2cae3508e1064faff33f734192fee3ed7a
Bernard Mutinda <164620456+BkMutinda@users.noreply.github.com>
2024-10-31 17:32:48 +0300

    Update system.js

--- system.js
+++ system.js
@@ -1,33 +1,84 @@
-document.querySelector('.button').addEventListener('click', function(){
-    const num = parseInt(document.getElementById('number').value);
+        document.querySelector('.button').addEventListener('click', function(){    
+            const selectElement = document.getElementById('types');
+            const selectedValue = selectElement.value;
+            const num = parseInt(document.getElementById('number').value);
 
-    const selectElement = document.getElementById('types');
-    const selectedValue = selectElement.value;
+    if(selectedValue === "decimal"){
+
+        const decimal = num.toString(10);
+        const binary = num.toString(2);
+        const octal =num.toString(8);
+        const hexadecimal = num.toString(16);
+
+        alert("you selected a decimal");
+        document.getElementById('output').innerText = 
+        `the representation of ${num} in decimal is :  ${decimal}\n`+
+         `the representation of ${num} in binary is :  ${binary}\n`+
+         `the representation of ${num} in octal is :  ${octal} \n`+
+         `the representation of ${num} in hexadecimal is :  ${hexadecimal}\n `;
+
+         document.getElementById('ans').innerText = 
+         `${decimal}\nBinary: ${binary}\nOctal: ${octal}\nHexadecimal: ${hexadecimal}`;
+
+        
+    }
+    if(selectedValue === "binary"){
+
+        const decimal = num.toString(10);
+        const binary = num.toString(2);
+        const octal =num.toString(8);
+        const hexadecimal = num.toString(16);
+
+        alert("you selected a binary");
+        document.getElementById('output').innerText = 
+        `the representation of ${num} in decimal is :  ${decimal}\n`+
+         `the representation of ${num} in binary is :  ${binary}\n`+
+         `the representation of ${num} in octal is :  ${octal} \n`+
+         `the representation of ${num} in hexadecimal is :  ${hexadecimal}\n `;
+
+        
+    }
+    
+    if(selectedValue === "octal"){
+
+        const decimal = num.toString(10);
+        const binary = num.toString(2);
+        const octal =num.toString(8);
+        const hexadecimal = num.toString(16);
+
+        alert("you selected a octal");
+
+
+        document.getElementById('output').innerText = 
+        `the representation of ${num} in decimal is :  ${decimal}\n`+
+         `the representation of ${num} in binary is :  ${binary}\n`+
+         `the representation of ${num} in octal is :  ${octal} \n`+
+         `the representation of ${num} in hexadecimal is :  ${hexadecimal}\n `;
+
+        
+    }
+    if(selectedValue === "binary"){
+
+        const decimal = num.toString(10);
+        const binary = num.toString(2);
+        const octal =num.toString(8);
+        const hexadecimal = num.toString(16);
+
+        alert("you selected a binary");
+
+        
+        document.getElementById('output').innerText = 
+        `the representation of ${num} in decimal is :  ${decimal}\n`+
+         `the representation of ${num} in binary is :  ${binary}\n`+
+         `the representation of ${num} in octal is :  ${octal} \n`+
+         `the representation of ${num} in hexadecimal is :  ${hexadecimal}\n `;
+
+        
+    }
+
     if (selectedValue === " ") {
         alert("OOPS!!, SOMETHING WENT WRONG... PLEASE MAKE A SELECTION. ");
         document.getElementById('output').innerText = " Ensure to make a selection";
 
         exit;
     }
-
-    const decimal = num.toString(10);
-    const binary = num.toString(2);
-    const octal = num.toString(8);
-    const hexadecimal = num.toString(16);
-
-
-if (`${num}` > 0 ) {
-    document.getElementById('output').innerText = 
-    `the representation of ${num} in decimal is :  ${decimal}\n`+
-     `the representation of ${num} in binary is :  ${binary}\n`+
-     `the representation of ${num} in octal is :  ${octal} \n`+
-     `the representation of ${num} in hexadecimal is :  ${hexadecimal}\n`;
-        
-
-    }else if (isNaN(num) ) {
-
-        document.getElementById('output').innerText = " Wrong input....\n\n Enter a valid number";
-    }
-
-});
-
